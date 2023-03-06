3ad4b0f3c0db100cf0fefadb3d26fec156b5492d
Leidy Quiñones <lalis.181014@gmail.com>
2023-03-06 13:55:11 -0500

    se agregó un archivo js

---
+++ programa.js
@@ -0,0 +1,13 @@
+//comentario de una línea
+//Para poner el usuario y correo para cambios de git se usa:
+//git config -g user.name. "..." si es global y no tener que cambiarla siempre
+/*comentarios
+de
+varias
+líneas*/
+//En javascript no es necesario declarar la variable antes de utilizarla
+
+//variable para ámbito de función
+var a;
+// variable de ámbito de bloque
+let a;
